86b313b2754747f6f6adf97010894e8e173570a4
Joko Supriyanto <jokomanza@gmail.com>
2024-05-05 19:47:07 +0700

    Update sample code in main method

--- src/Main.java
+++ src/Main.java
@@ -14,15 +14,28 @@ public class Main {
      * @param args Command line arguments
      */
     public static void main(String[] args) {
-        Langkah duaLangkah = Langkah.DUA_LANGKAH;
-        Mesin mesinSepedaMotor = new Mesin(2, 500, duaLangkah);
-        SepedaMotor sepedaMotor = new SepedaMotor(2, mesinSepedaMotor);
+        Mesin mesinHondaCbr250rr = new Mesin(2, 249.7f, Langkah.EMPAT_LANGKAH);
+        SepedaMotor hondaCbr250rr = new SepedaMotor(mesinHondaCbr250rr);
+        hondaCbr250rr.setBahanBakar(14.5f);
+        hondaCbr250rr.setKetinggianTempatDuduk(790);
 
-        Langkah empatLangkah = Langkah.EMPAT_LANGKAH;
-        Mesin mesinMobil = new Mesin(4, 1000, empatLangkah);
-        Mobil mobil = new Mobil(4, mesinMobil);
+        Mesin mesinSupraMk4 = new Mesin(6, 2997f, Langkah.EMPAT_LANGKAH);
+        Mobil supraMk4 = new Mobil(mesinSupraMk4);
+        supraMk4.setBahanBakar(70f);
+        supraMk4.setJumlahPintu(2);
 
-        System.out.println("MotorCycle with " + sepedaMotor.getBahanBakar() + " fuels has a " + sepedaMotor.getEngine().getJumlahSilinder() + "-cylinder engine with a capacity of " + sepedaMotor.getEngine().getKapasitasSilinder() + " cc and a " + sepedaMotor.getEngine().getStroke() + " stroke");
-        System.out.println("Car with " + mobil.getBahanBakar() + " fuels has a " + mobil.getEngine().getJumlahSilinder() + "-cylinder engine with a capacity of " + mobil.getEngine().getKapasitasSilinder() + " cc and a " + mobil.getEngine().getStroke() + " stroke");
+        System.out.println("Sepeda Motor Honda CBR250RR");
+        System.out.println("Bahan bakar: " + hondaCbr250rr.getBahanBakar() + " liter");
+        System.out.println("Jumlah silinder: " + hondaCbr250rr.getEngine().getJumlahSilinder());
+        System.out.println("Kapasitas silinder: " + hondaCbr250rr.getEngine().getKapasitasSilinder() + " cc");
+        System.out.println("Langkah: " + hondaCbr250rr.getEngine().getLangkah());
+        System.out.println("Ketinggian tempat duduk: " + hondaCbr250rr.getKetinggianTempatDuduk() + " mm");
+
+        System.out.println("\nMobil Toyota Supra MK4");
+        System.out.println("Bahan bakar: " + supraMk4.getBahanBakar() + " liter");
+        System.out.println("Jumlah silinder: " + supraMk4.getEngine().getJumlahSilinder());
+        System.out.println("Kapasitas silinder: " + supraMk4.getEngine().getKapasitasSilinder() + " cc");
+        System.out.println("Langkah: " + supraMk4.getEngine().getLangkah());
+        System.out.println("Jumlah pintu: " + supraMk4.getJumlahPintu());
     }
 }
